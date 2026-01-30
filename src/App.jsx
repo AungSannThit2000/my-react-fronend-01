c48c4c58d0f0d8eb03664aeb15d64ed3a29842eb
AungSannThit2000 <astshiroe@gmail.com>
2026-01-30 15:37:00 +0700

    CRUD update function for items

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useEffect, useState } from 'react';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import TestAPI from './components/TestAPI';
-import './App.css'
+import Items from './components/Items';
+import ItemDetail from './components/ItemDetail';
+import './App.css';
+
 function App() {
-  
   return (
     <Routes>
-      <Route path='/test_api' element={<TestAPI/>}/>
+      <Route path='/test_api' element={<TestAPI />} />
+      <Route path='/items' element={<Items />} />
+      <Route path='/items/:id' element={<ItemDetail />} />
     </Routes>
-  )
+  );
 }
-export default App
+
+export default App;
