47ab50dc383ff54574c8b0208c513dd454cea0c8
AungSannThit2000 <astshiroe@gmail.com>
2026-02-07 22:43:00 +0700

    Add auth routes and wrap app with UserProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 import TestAPI from './components/TestAPI';
 import Items from './components/Items';
 import ItemDetail from './components/ItemDetail';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
+import UserList from './components/UserList';
+import RequireAuth from './middleware/RequireAuth';
 import './App.css';
 
 function App() {
@@ -10,6 +15,31 @@ function App() {
       <Route path='/test_api' element={<TestAPI />} />
       <Route path='/items' element={<Items />} />
       <Route path='/items/:id' element={<ItemDetail />} />
+      <Route path='/login' element={<Login />} />
+      <Route
+        path='/profile'
+        element={
+          <RequireAuth>
+            <Profile />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path='/logout'
+        element={
+          <RequireAuth>
+            <Logout />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path='/users'
+        element={
+          <RequireAuth>
+            <UserList />
+          </RequireAuth>
+        }
+      />
     </Routes>
   );
 }
